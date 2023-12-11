342b67516b5a5d2978816629b1d45fbbe5387a9d
Marouane Bouaricha <marouane@itchallenge.fr>
2023-12-11 12:37:04 +0100

    Added post and get routes

--- server.go
+++ server.go
@@ -14,6 +14,8 @@ func main() {
 	router.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
 		fmt.Fprintln(resp, "Up and running...")
 	})
+	router.HandleFunc("/posts", getPosts).Methods("GET")
+	router.HandleFunc("/posts", addPost).Methods("POST")
 	log.Println("Server listening on port ", port)
 	log.Fatalln(http.ListenAndServe(port, router))
 }
