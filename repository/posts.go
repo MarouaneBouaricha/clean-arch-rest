e0bef442586094a850d2172b1f6077871526a915
Marouane Bouaricha <marouane@itchallenge.fr>
2023-12-11 15:57:39 +0100

    Added implementation of save and post methods

---
+++ repository/posts.go
@@ -0,0 +1,70 @@
+package repository
+
+import (
+	"clean-arch/models"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "tux"
+	password = "mysecretpassword"
+	dbname   = "blog"
+)
+
+type PostRepository interface {
+	Save(post *models.Post) (bool, error)
+	FindAll() ([]models.Post, error)
+}
+
+type repo struct{}
+
+func NewPostRepository() PostRepository {
+	return &repo{}
+}
+
+func (*repo) Save(post *models.Post) (bool, error) {
+	db, err := connectToPostgreSQL()
+	if err != nil {
+		return false, err
+	}
+	// Perform database migration
+	err = db.AutoMigrate(&models.Post{})
+	if err != nil {
+		return false, err
+	}
+
+	err = db.Create(post).Error
+	if err != nil {
+		return false, err
+	}
+	log.Println("Created Post:", post)
+	return true, nil
+}
+
+func (*repo) FindAll() ([]models.Post, error) {
+	db, err := connectToPostgreSQL()
+	if err != nil {
+		return nil, err
+	}
+	var posts []models.Post
+	result := db.Find(&posts)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return posts, nil
+}
+
+func connectToPostgreSQL() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
