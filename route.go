8517b902fcf25efcec71077b979101a86a543dd5
Marouane Bouaricha <marouane@itchallenge.fr>
2023-12-11 12:36:29 +0100

    Added add and get methods

---
+++ route.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Post struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Text  string `json:"text"`
+}
+
+var posts []Post
+
+func init() {
+	posts = []Post{
+		Post{
+			Id:    1,
+			Title: "Post 1",
+			Text:  "Post text 1",
+		},
+	}
+}
+
+func getPosts(resp http.ResponseWriter, req *http.Request) {
+	resp.Header().Set("Content-type", "application/json")
+	result, err := json.Marshal(posts)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error marshelling the posts array"}`))
+		return
+	}
+	resp.WriteHeader(http.StatusOK)
+	resp.Write(result)
+}
+
+func addPost(resp http.ResponseWriter, req *http.Request) {
+	var post Post
+	resp.Header().Set("Content-type", "application/json")
+	err := json.NewDecoder(req.Body).Decode(&post)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error marshelling the posts array"}`))
+		return
+	}
+	post.Id = len(posts) + 1
+	posts = append(posts, post)
+	resp.WriteHeader(http.StatusOK)
+	result, err := json.Marshal(post)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error": "Error marshelling post object"}`))
+		return
+	}
+	resp.Write(result)
+}
